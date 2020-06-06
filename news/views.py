d82de425d2e60a3667e34131d371bd01e5255d9f
Anton Shostak <60942970+ShostakAnton@users.noreply.github.com>
2020-06-06 23:00:31 +0300

    вывод комментариев к статье

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import News
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import News, Comments
+from .forms import CommentForm
 
 
 def news_list(request):
@@ -11,4 +12,18 @@ def news_list(request):
 
 def new_single(request, pk):
     new = get_object_or_404(News, id=pk)
-    return render(request, "news/new_single.html", {"new": new})
+    comment = Comments.objects.filter(new=pk, moderation=True)
+    if request.method == 'POST':    # если POST запрос мы отправляем заполненую форму
+        form = CommentForm(request.POST)    # пользователь уже ввел комментарий
+        if form.is_valid():     # проверка на коректность
+            form = form.save(commit=False)     # приостановление сохранение формы
+            form.user = request.user    # присваеваем пользователя который сейчас на сайте
+            form.new = new      # присваиваем комментарий текущей статье
+            form.save()     # сохранение формы
+            return redirect('new_single', pk)   # перенаправление на текущею страницу
+    else:                     # если у нас GET запрос мы отображаем пустую форму
+        form = CommentForm()
+    return render(request, "news/new_single.html",
+                  {"new": new,
+                   'comments': comment,
+                   'form': form, })
