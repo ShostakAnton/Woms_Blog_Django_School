7302b49bfe90feffdaaa51e484400f584e4083fa
Anton Shostak <60942970+ShostakAnton@users.noreply.github.com>
2020-06-07 23:52:08 +0300

    add static

--- Woms_Blog_Django_School/settings.py
+++ Woms_Blog_Django_School/settings.py
@@ -133,7 +133,10 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
